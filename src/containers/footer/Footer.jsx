5cb9ec0cdeb91ccc08f527dd83cbd3dad86e0b42
uceiifali <mussicxxx@gmail.com>
2022-01-25 18:35:47 +0200

    .

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./footer.css";
+import gpt3Logo from "../../assets/logo.svg";
+const Footer = () => {
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="gpt3_logo" />
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions </p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Uceiif Ali</p>
+          <p>+(20) 112-687-1615</p>
+          <p>Uceiifali@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./header.css";
+import People from "../../assets/people.png";
+import AI from "../../assets/ai.png";
+
+const Header = () => {
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let&apos;s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="Email" placeholder="You E-mail" />
+          <button type="button"> Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={People} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={AI} alt="ai" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
